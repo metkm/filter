7109c19ec5c706c6b5f32e17e037eeccdab9d90f
metkm <metinkorkmaz417@gmail.com>
2025-04-22 20:37:07 +0300

    test

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #[cfg(not(test))]
 extern crate wdk_panic;
 
+use wdk::println;
 #[cfg(not(test))]
 use wdk_alloc::WdkAllocator;
 
@@ -10,22 +11,22 @@ use wdk_alloc::WdkAllocator;
 #[global_allocator]
 static GLOBAL_ALLOCATOR: WdkAllocator = WdkAllocator;
 
-use wdk_sys::{DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, PDRIVER_OBJECT, ntddk::IoDeleteDevice};
+use wdk_sys::{DRIVER_OBJECT, NTSTATUS, PCUNICODE_STRING, PDRIVER_OBJECT};
 
-unsafe extern "C" fn boost_unload(driver: *mut DRIVER_OBJECT) {
-    unsafe {
-        IoDeleteDevice((*driver).DeviceObject);
-    }
+unsafe extern "C" fn filter_unload(_driver: *mut DRIVER_OBJECT) {
+    println!("unloading driver");
 }
 
 #[unsafe(export_name = "DriverEntry")] // WDF expects a symbol with the name DriverEntry
 pub unsafe extern "system" fn driver_entry(
-    driver: &PDRIVER_OBJECT,
+    driver: PDRIVER_OBJECT,
     _registry_path: PCUNICODE_STRING,
 ) -> NTSTATUS {
     wdk::println!("driver entry");
 
-    (unsafe { *(*driver) }).DriverUnload = Some(boost_unload);
+    unsafe {
+        (*driver).DriverUnload = Some(filter_unload);
+    }
 
     0
 }
